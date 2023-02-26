28b901d12b8b0a4952eb4fb3ee76f57e3046d157
zaid <zaid97abyi@gmail.com>
2023-02-26 16:29:26 +0300

    Fix

--- Blazor.Learner/Server/Program.cs
+++ Blazor.Learner/Server/Program.cs
@@ -14,28 +14,28 @@ public class Program
     /// Defines the entry point of the application.
     /// </summary>
     /// <param name="args">The arguments.</param>
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
         var host = CreateHostBuilder(args).Build();
 
-        using (var scope = host.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-            try
-            {
-                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, roleManager);
-            }
-            catch (Exception ex)
-            {
-                var logger = loggerFactory.CreateLogger<Program>();
-                logger.LogError(ex, "An error occurred seeding the DB.");
-            }
-        }
+        //using (var scope = host.Services.CreateScope())
+        //{
+        //    var services = scope.ServiceProvider;
+        //    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+        //    try
+        //    {
+        //        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        //        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        //        await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, roleManager);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var logger = loggerFactory.CreateLogger<Program>();
+        //        logger.LogError(ex, "An error occurred seeding the DB.");
+        //    }
+        //}
 
-        await host.RunAsync();
+        host.Run();
     }
 
     /// <summary>
